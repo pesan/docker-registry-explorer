49f44428c89e5affe6ff48099564acaf4de64429
pesan <pesan@users.noreply.github.com>
2015-04-02 17:16:30 +0200

    Simplify the server version component

--- server/components/version/index.js
+++ server/components/version/index.js
@@ -4,12 +4,6 @@ var fs = require('fs');
 
 module.exports = function(app) {
 	return function(req, res) {
-		fs.readFile(app.get('appPath') + '/version.json', 'utf8', function(err, data) {
-			if (err) {
-				res.status(404).end('Not found');
-			} else {
-				res.type('json').send(data);
-			}
-		});
+		res.sendfile(app.get('appPath') + '/version.json');
 	};
 };
